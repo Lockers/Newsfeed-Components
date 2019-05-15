4a325dd37c2fa04541095cc02e41d9215a4f15f3
Matt Locklin <mattlocklin@live.co.uk>
2019-05-15 14:27:17 +0100

    Completed class apart form articleMEthod FIleld in all the excercises for the comments just now trying to figure out how to make the articles expand

--- Article/Article.js
+++ Article/Article.js
@@ -1,20 +1,24 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
+  
   constructor(domElement) {
+    // debugger
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
+
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector('.expandButton');
+
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'Click if you suck';
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this));
   }
-
   expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    // debugger
+    this.domElement.classList.toggle('.article-open');
   }
 }
 
@@ -26,4 +30,8 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+
+articles.forEach((article) => {
+  new Article(article);
+})
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -11,6 +11,4 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button')
 menuButton.addEventListener('click', toggleMenu);
 
-
-// .addEventListener('click', toggleMenu());
 // Using your menuButton reference, add a click handler that calls toggleMenu
