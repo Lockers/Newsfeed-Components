5dac0866e069b58a18e82b68ebea842ebe0cbd09
Matt Locklin <mattlocklin@live.co.uk>
2019-05-15 13:15:12 +0100

    Finished first task
    added element selectors and toggle fucntion to make menu appear on event
    click

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,16 @@
 
 const toggleMenu = () => {
+  
+  menu.classList.toggle('menu--open');
   // Toggle the "menu--open" class on your menu refence. 
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
-// create a reference to the ".menu-button" class
-const menuButton;
+const menu = document.querySelector('.menu');
+
+const menuButton = document.querySelector('.menu-button')
+menuButton.addEventListener('click', toggleMenu);
+
+
+// .addEventListener('click', toggleMenu());
 // Using your menuButton reference, add a click handler that calls toggleMenu
